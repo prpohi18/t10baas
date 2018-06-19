9cbc9b15b3b30d25246458e1a039226a83467c35
Kaur K <kaurk@users.noreply.github.com>
2018-06-19 10:57:32 +0300

    Connection established, data received

--- baastest3/src/java/BaasTEST.java
+++ baastest3/src/java/BaasTEST.java
@@ -3,29 +3,44 @@ import java.sql.*;
 public class BaasTEST{
     
     
-    //MySQL Workbenchis on local instance, user:root pass: XXX, port 3306
+    //MySQL Workbenchis on local instance, user:root pass: XXX, port 3306, baas: test1, tabel:nimi
     
     public static void main(String[] arg) throws Exception{        
        // System.out.println("Proovin sisse logida:");
         Connection conn = null;
         try{
-            DriverManager.getConnection(
-             "jdbc:mysql://localhost:3306/test1","root","b1m2w3");
-                if(conn !=null){
-                    System.out.println("Connected!");   
-                    Statement st=conn.createStatement();
-                    ResultSet rs=st.executeQuery("SELECT nimi FROM nimed");
-                    while(rs.next()){
-                    System.out.println(rs.getString("nimi"));
+            conn = DriverManager.getConnection(
+             "jdbc:mysql://localhost:3306/test1","root","b1m2w3");              //ühendun baasiga
+                if(conn!=null){
+                    System.out.println("Connection established!");              //annab teada, et on ühendatud
+                    Statement st=conn.createStatement();                        //saadan päringu
+                    ResultSet rs=st.executeQuery("SELECT nimi FROM nimed");     //küsin tabelist nime
+                    while(rs.next()){                                           //kui on nimi olemas
+                        System.out.println("Tabelis olev nimi: "+rs.getString("nimi"));                   //annab mulle siin nime kätte
                     }
                     
                 }
-        }
-            
-        catch(Exception e){
+        } catch (Exception e){
             System.out.println("NOT connected!");
             }
-    }
-        
+     System.out.println(conn);    
+   }
 }
        
+/*-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+run:
+Connection established!
+Tabelis olev nimi: Juku
+Tabelis olev nimi: Miku
+com.mysql.jdbc.JDBC4Connection@6f2b958e
+BUILD SUCCESSFUL (total time: 1 second)
+*/
+
+/*  -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   
+SQL käsklused
+
+CREATE TABLE nimed (nimi VARCHAR(10));
+INSERT INTO nimed (nimi) VALUES ("Juku");
+INSERT INTO nimed (nimi) VALUES ("Miku");
+
+*/
