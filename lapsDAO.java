507fc17c9cabc49a5064f567ea2f847b2ad84d34
Krislyn Luks <krsln@tlu.ee>
2018-06-19 13:49:49 +0300

    Delete lapsDAO.java

--- lapsDAO.java
+++
@@ -1,6 +0,0 @@
-package krislyn.andmebaas;
-
-import javax.transaction.*;
-import org.springframework.data.repository.CrudRepository;
-@Transactional
-public interface lapsDAO extends CrudRepository<Laps, Integer>{}
